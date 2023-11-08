aeadc5eede3f0d2e5d6284ef08e9e0453df327dd
hertg <3533648+hertg@users.noreply.github.com>
2023-11-08 21:40:55 +0100

    update nvim config

--- roles/base/files/userconfig/nvim/after/plugin/telescope.lua
+++ roles/base/files/userconfig/nvim/after/plugin/telescope.lua
@@ -4,6 +4,9 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set('n', '<leader>pg', function()
+	builtin.live_grep();
+end)
 
 
 -- source: https://github.com/nvim-telescope/telescope-ui-select.nvim
